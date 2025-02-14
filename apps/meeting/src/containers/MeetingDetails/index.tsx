1b238ed1f63788d134609f18cc09f8efe126cc2d
ymktmk <73768462+ymktmk@users.noreply.github.com>
2025-02-14 19:55:11 +0900

    modify: font-size

--- apps/meeting/src/containers/MeetingDetails/index.tsx
+++ apps/meeting/src/containers/MeetingDetails/index.tsx
@@ -9,7 +9,7 @@ const MeetingDetails = () => {
   return (
     <Flex container layout="fill-space-centered">
       <Flex mb="2rem" mr={{ md: '2rem' }} px="1rem">
-        <span style={{ fontSize: '20px', lineHeight: '1.5' }}>
+        <span style={{ fontSize: '16px', lineHeight: '1.5' }}>
           ＊別の診察状況により医師またはカウンセラーの入室が遅れる可能性がございます。
           ご入室のままでお待ち下さい。診察予約時間から5分以上経っても入室がない場合はLINEまでご連絡いただけますと幸いです。
         </span>
